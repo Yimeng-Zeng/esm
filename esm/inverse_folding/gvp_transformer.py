45b6ad55e211b245499796980f3813e5755d74fa
yimengz <yimengz@seas.upenn.edu>
2023-05-10 15:39:08 -0400

    wip

--- esm/inverse_folding/gvp_transformer.py
+++ esm/inverse_folding/gvp_transformer.py
@@ -226,3 +226,78 @@ class GVPTransformerModel(nn.Module):
         
         # Convert back to string via lookup
         return all_seqs
+
+
+    def sample_batch2(self, coords, num_samples=1, partial_seq=None, temperature=1.0, confidence=None, device=None):
+        """
+        Samples sequences based on multinomial sampling (no beam search).
+
+        Args:
+            coords: L x 3 x 3 list representing one backbone
+            partial_seq: Optional, partial sequence with mask tokens if part of
+                the sequence is known
+            temperature: sampling temperature, use low temperature for higher
+                sequence recovery and high temperature for higher diversity
+            confidence: optional length L list of confidence scores for coordinates
+        """
+        L = len(coords)
+        # Convert to batch format
+        batch_converter = CoordBatchConverter(self.decoder.dictionary)
+        batch = [(coords, confidence, None)] * num_samples
+        batch_coords, confidence, _, _, padding_mask = (
+            batch_converter(batch, device=device)
+        )
+        
+        # Start with prepend token
+        mask_idx = self.decoder.dictionary.get_idx('<mask>')
+        sampled_tokens = torch.full((1, 1+L), mask_idx, dtype=int)
+        sampled_tokens[0, 0] = self.decoder.dictionary.get_idx('<cath>')
+        if partial_seq is not None:
+            for i, c in enumerate(partial_seq):
+                sampled_tokens[0, i+1] = self.decoder.dictionary.get_idx(c)
+            
+        # Save incremental states for faster sampling
+        incremental_state = dict()
+        
+        # Run encoder only once
+        all_encoder_out = self.encoder(batch_coords, padding_mask, confidence)
+        
+        # Make sure all tensors are on the same device if a GPU is present
+        if device:
+            sampled_tokens = sampled_tokens.to(device)
+
+        # Get encoder output for all samples
+        encoder_out = {}
+        encoder_out['encoder_out'] = [all_encoder_out['encoder_out'][0]]
+        encoder_out['encoder_padding_mask'] = [all_encoder_out['encoder_padding_mask'][0]]
+        for key in all_encoder_out.keys():
+            if key != 'encoder_out' and key != 'encoder_padding_mask':
+                try:
+                    encoder_out[key] = all_encoder_out[key]
+                except:
+                    encoder_out[key] = all_encoder_out[key]
+
+        # Decode one token at a time
+        for i in range(1, L+1):
+            logits, _ = self.decoder(
+                sampled_tokens[:, :i], 
+                encoder_out,
+                incremental_state=incremental_state,
+            )
+            logits = logits.transpose(0, 1)
+            logits /= temperature
+            probs = F.softmax(logits, dim=-1)
+            sampled_tokens[:, i] = torch.where(sampled_tokens[:, i] == mask_idx, 
+                                            torch.multinomial(probs, 1).squeeze(-1), 
+                                            sampled_tokens[:, i])
+
+        # Convert tokens to strings
+        all_seqs = []
+        for sample_idx in range(num_samples):
+            sampled_seq = sampled_tokens[sample_idx, 1:]
+            sampled_seq = ''.join([self.decoder.dictionary.get_tok(a) for a in sampled_seq])
+            all_seqs.append(sampled_seq)
+
+        
+        # Convert back to string via lookup
+        return all_seqs
